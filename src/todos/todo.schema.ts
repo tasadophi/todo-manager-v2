b42dbbcbf8a323fde6182726280ee43fbc069fc1
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2024-12-29 22:26:57 +0330

    feat: add config and todo schema

---
+++ src/todos/todo.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type TodoDocument = HydratedDocument<Todo>;
+
+@Schema()
+export class Todo {
+  @Prop({ required: [true, "title is required !"] })
+  title: string;
+
+  @Prop({ required: [true, "description is required !"] })
+  description: string;
+
+  @Prop()
+  isDone: boolean;
+
+  @Prop()
+  isFavorite: boolean;
+}
+
+export const TodoSchema = SchemaFactory.createForClass(Todo);
