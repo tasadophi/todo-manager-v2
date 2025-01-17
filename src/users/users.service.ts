b2ac0bec2fe43376026d8f99c8d9a5db31ca5dfd
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2025-01-17 22:21:13 +0330

    feat: implement create user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,26 @@
 import { Model } from "mongoose";
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { User } from "@/users/schemas/user.schema";
+import { User, UserDocument } from "@/users/schemas/user.schema";
+import { SignupDto } from "@/auth/dtos/signup.dto";
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(user: SignupDto) {
+    const { email } = user;
+    const foundUser = await this.userModel.findOne({ email });
+    if (foundUser) {
+      throw new HttpException("user already exists", HttpStatus.BAD_REQUEST);
+    }
+    const createdUser = await this.userModel.create(user);
+    return this.sanitizeUser(createdUser);
+  }
+
+  sanitizeUser(user: UserDocument) {
+    const sanitized = user.toObject();
+    delete sanitized["password"];
+    return sanitized;
+  }
 }
