08e9df56081a069e20d572457806dc315d335e9d
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2025-01-17 22:19:20 +0330

    feat: use bcrypt for hash password on save

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { HydratedDocument } from "mongoose";
+import * as bcrypt from "bcrypt";
 
 export type UserDocument = HydratedDocument<User>;
 
@@ -12,7 +13,23 @@ export class User {
   name: string;
 
   @Prop({ required: [true, "password is required !"] })
-  password: string;
+  password?: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+    if (!this.password) {
+      return next(new Error("password is required"));
+    }
+    const hashed = await bcrypt.hash(this.password, 10);
+    this.password = hashed;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
